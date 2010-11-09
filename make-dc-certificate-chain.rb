085dca31473fd1fae1faa9ae72a68286f023de7a
Wolfgang Woehl <tito@online.de>
2010-11-09 13:11:53 +0100

    make-dc-certificate-chain.rb: Comments

--- make-dc-certificate-chain.rb
+++ make-dc-certificate-chain.rb
@@ -1,4 +1,5 @@
 #!/usr/bin/env ruby
+# Requires ruby and openssl
 #
 # Make a digital cinema compliant certificate chain
 # Proof of concept. In real-world applications you'd want 
@@ -35,9 +36,10 @@ old.each do |file|
   File.delete( file )
 end
 
-# Generate CA key and certificate
+### Make a self-signed root certificate. This will act as trust anchor for your certificate chain
+# Generate CA key
 `openssl genrsa -out ca.key 2048`
-
+# Note basicConstraints and keyUsage
 ca_cnf = <<EOF
 [ req ]
 distinguished_name = req_distinguished_name
@@ -53,17 +55,17 @@ OU = Organization unit
 CN = Entity and dnQualifier
 EOF
 File.open( 'ca.cnf', 'w' ) { |f| f.write( ca_cnf ) }
-
-# Self-sign CA certificate
 # Subject dnQualifier:
 ca_dnq = `openssl rsa -outform PEM -pubout -in ca.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
 ca_dnq = ca_dnq.gsub( '/', '\/' ) # can have values like '0Za8/aABE05Aroz7le1FOpEdFhk=', note the '/'. protect for name parser
 # The following simply concatenates the dnq. Somehow I have a feeling that that's not quite right. Someone knows?
 ca_subject = '/O=example.com/OU=csc.example.com/CN=dcstore.ROOT/dnQualifier=' + ca_dnq
+# Generate self-signed certificate
 `openssl req -new -x509 -sha256 -config ca.cnf -days 365 -set_serial 5 -subj "#{ ca_subject }" -key ca.key -outform PEM -out ca.self-signed.pem`
+###
 
-
-# Generate intermediate key and certificate
+### Make an intermediate certificate, issued/signed by the root certificate
+# Generate intermediate's key
 `openssl genrsa -out intermediate.key 2048`
 # Note basicConstraints pathlen
 inter_cnf = <<EOF
@@ -81,17 +83,17 @@ OU = Organization unit
 CN = Entity and dnQualifier
 EOF
 File.open( 'intermediate.cnf', 'w' ) { |f| f.write( inter_cnf ) }
-
-# Request signing for intermediate certificate
 inter_dnq = `openssl rsa -outform PEM -pubout -in intermediate.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
 inter_dnq = inter_dnq.gsub( '/', '\/' )
 inter_subject = "/O=example.com/OU=csc.example.com/CN=dcstore.INTERMEDIATE/dnQualifier=" + inter_dnq
+# Request signing for intermediate certificate
 `openssl req -new -config intermediate.cnf -days 365 -subj "#{ inter_subject }" -key intermediate.key -out intermediate.csr`
-# Sign
+# Sign with root certificate
 `openssl x509 -req -sha256 -days 365 -CA ca.self-signed.pem -CAkey ca.key -set_serial 6 -in intermediate.csr -extfile intermediate.cnf -extensions v3_ca -out intermediate.signed.pem`
+###
 
-
-# Generate leaf certificate. Will be used to sign CPLs and PKLs
+### Make a leaf certificate, issued/signed by the intermediate certificate. Use the leaf certificate to sign content (like CPLs and PKLs)
+# Generate leaf's key
 `openssl genrsa -out leaf.key 2048`
 # Note basicConstraints and keyUsage
 leaf_cnf = <<EOF
@@ -109,20 +111,19 @@ OU = Organization unit
 CN = Entity and dnQualifier
 EOF
 File.open( 'leaf.cnf', 'w' ) { |f| f.write( leaf_cnf ) }
-
-# Request signing for leaf certificate
 leaf_dnq = `openssl rsa -outform PEM -pubout -in leaf.key | openssl base64 -d | dd bs=1 skip=24 2>/dev/null | openssl sha1 -binary | openssl base64`.chomp
 leaf_dnq = leaf_dnq.gsub( '/', '\/' )
-# Note CN's role signifier CS (Content signer/creator)
+# Note the CN role signifier CS (Content signer/creator)
 leaf_subject = "/O=example.com/OU=csc.example.com/CN=CS dcstore.LEAF/dnQualifier=" + leaf_dnq
+# Request signing for leaf certificate
 `openssl req -new -config leaf.cnf -days 365 -subj "#{ leaf_subject }" -key leaf.key -outform PEM -out leaf.csr`
-# Sign
+# Sign with intermediate certificate
 `openssl x509 -req -sha256 -days 365 -CA intermediate.signed.pem -CAkey intermediate.key -set_serial 7 -in leaf.csr -extfile leaf.cnf -extensions v3_ca -out leaf.signed.pem`
+###
 
-
+### Print out issuer and subject information for each of the generated certificates
 puts "\n+++ Certificate info +++\n"
 [ [ 'Self-signed CA certificate (issuer == subject)', 'ca.self-signed.pem' ], [ 'Intermediate certificate', 'intermediate.signed.pem' ], [ 'Leaf certificate', 'leaf.signed.pem' ] ].each do |certificate|
   puts "\n#{ certificate.first } (#{ certificate.last }):\n#{ `openssl x509 -noout -subject -in #{ certificate.last }` }   signed by\n #{ `openssl x509 -noout -issuer -in #{ certificate.last }` }"
 end
-
 
