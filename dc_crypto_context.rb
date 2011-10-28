981e118fa6293ebf3478bcfd15371200c01e7af5
Wolfgang Woehl <tito@online.de>
2011-10-28 09:09:20 +0200

    dc_crypto_context.rb: Licensed under the terms and conditions of GPLv3. See gpl.txt

--- dc_crypto_context.rb
+++ dc_crypto_context.rb
@@ -1,6 +1,23 @@
 #!/usr/bin/env ruby
 #
-# Wolfgang Woehl v0.2011.04.09
+#
+# dc_crypto_context.rb checks X509 certificates for SMPTE 430-2 compliance
+# Copyright 2011 Wolfgang Woehl
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+AppVersion = 'v0.2011.04.09'
 #
 # Script will check a given set of files for the presence of a SMPTE compliant certificate chain, used in 
 # digital cinema to sign Composition Playlists (CPL), Packing Lists (PKL) and Key Delivery Messages (KDM).
